35724019a9ff0ff0d220338d181eb7c991852c41
alexn11 <alexn11.gh@gmail.com>
2019-05-19 11:41:57 +0100

    rm batch_size and other simplifications + new parameters

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -22,12 +22,10 @@ def interact_model(
     model_name='117M',
     seed=None,
     nsamples=1,
-    batch_size=1,
     length=None,
     temperature=1,
     mode="random",
     key="The Library Of Babel!",
-    top_k=0,
     models_dir='models',    
 ):
     """
@@ -36,7 +34,6 @@ def interact_model(
     :seed=None : Integer seed for random number generators, fix seed to reproduce
      results
     :nsamples=1 : Number of samples to return total
-    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
     :length=None : Number of tokens in generated text, if None (default), is
      determined by model hyperparameters
     :temperature=1 : Float value controlling randomness in boltzmann
@@ -47,22 +44,13 @@ def interact_model(
      "input": input interactively as a sequence of integers,
      "random": according to a Poisson distribution whose parameter lambda is equal to temperature
     :key : primordial context text, used to initialize the generator
-    :top_k=0 : (IGNORED) Integer value controlling diversity. 1 means only 1 word is
-     considered for each step (token), resulting in deterministic completions,
-     while 40 means 40 words are considered at each step. 0 (default) is a
-     special setting meaning no restrictions. 40 generally is a good value.
-     :models_dir : path to parent folder containing model subfolders
+    :models_dir : path to parent folder containing model subfolders
      (i.e. contains the <model_name> folder)     
     """
     if (mode not in ("input", "random")):
      raise Exception ("mode should be either input or random")
 
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
-
-
-    if batch_size is None:
-        batch_size = 1
-    assert nsamples % batch_size == 0
 
     poisson_lambda = temperature
 
@@ -77,16 +65,17 @@ def interact_model(
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
     with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
-        address = tf . placeholder (tf . int32, [batch_size, length])
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
-        output = sample.sample_sequence(
-            hparams=hparams, length=length, address=address,
-            context=context,
-            batch_size=batch_size,
-            temperature=temperature, top_k=top_k
-        )
+        context = tf . placeholder (tf . int32, [None, ])
+        address = tf . placeholder (tf . int32, [length, ])
+
+        np . random . seed (seed)
+        tf . set_random_seed (seed)
+
+        output = sample . sample_sequence (hparams = hparams,
+                                           length = length,
+                                           address = address,
+                                           context = context,
+                                           temperature = temperature)
 
         saver = tf.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
@@ -95,10 +84,10 @@ def interact_model(
         context_tokens = enc . encode (key)
 
         while True:
-            address_text = input("Address prompt >>> ")
+            address_text = input ("Address prompt >>> ")
             while not address_text:
-                print('Prompt should not be empty!')
-                address_text = input("Address prompt >>> ")
+                print ('Prompt should not be empty!')
+                address_text = input ("Address prompt >>> ")
             # NOTE: temperature is now used as the lambda parameter of the poisson distribution
             if (mode == "input"):
               # FOR HAND MADE ADDRESS:
@@ -108,18 +97,16 @@ def interact_model(
               # maybe put a "end of text" token in the context?
               address_input = np . random . poisson (poisson_lambda, length)
               address_input = np . where (address_input < hparams . n_vocab, address_input, hparams . n_vocab)
-            generated = 0
-            for _ in range(nsamples // batch_size):
-                out = sess . run (output,
+
+            out = sess . run (output,
                                   feed_dict = {
-                                        context : [ context_tokens for _ in range (batch_size)],
-                                        address : [ address_input for _ in range (batch_size) ],
-                                   }) [ : , len (context_tokens) : ]
-                for i in range(batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
+                                        context : context_tokens,
+                                        address : address_input,
+                                   }) [ len (context_tokens) : ]
+
+            text = enc . decode (out)
+            print("=" * 40 + " SAMPLE : " + "=" * 40)
+            print(text)
             print("=" * 80)
 
 if __name__ == '__main__':
--- src/sample.py
+++ src/sample.py
@@ -2,39 +2,26 @@ import tensorflow as tf
 
 import model
 
-def top_k_logits(logits, k):
-    if k == 0:
-        # no truncation
-        return logits
-
-    def _top_k():
-        values, _ = tf.nn.top_k(logits, k=k)
-        min_values = values[:, -1, tf.newaxis]
-        return tf.where(
-            logits < min_values,
-            tf.ones_like(logits, dtype=logits.dtype) * -1e10,
-            logits,
-        )
-    return tf.cond(
-       tf.equal(k, 0),
-       lambda: logits,
-       lambda: _top_k(),
-    )
 
 
-def sample_sequence(*, hparams, length, address, start_token=None, batch_size=None, context=None, temperature=1, top_k=0):
+def sample_sequence(*, hparams, length, address, start_token=None, context=None, temperature=1):
     if start_token is None:
         assert context is not None, 'Specify exactly one of start_token and context!'
     else:
         assert context is None, 'Specify exactly one of start_token and context!'
-        context = tf.fill([batch_size, 1], start_token)
+        context = tf.fill([1, ], start_token)
 
     def step(hparams, tokens, past=None):
-        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
+        #print (">>step >> tokens="+str(tokens))
+        X = tf . expand_dims (tokens, 0)
+        #print (">>step>> X = "+str(X))
+        #print (">>step>> past="+str(past))
+  
+        lm_output = model . model (hparams = hparams, X = X, past = past, reuse = tf.AUTO_REUSE)
 
-        logits = lm_output['logits'][:, :, :hparams.n_vocab]
-        presents = lm_output['present']
-        presents.set_shape(model.past_shape(hparams=hparams, batch_size=batch_size))
+        logits = lm_output ['logits'][:, :, :hparams.n_vocab]
+        presents = lm_output ['present']
+        presents . set_shape (model.past_shape(hparams=hparams, batch_size=1))
         return {
             'logits': logits,
             'presents': presents,
@@ -44,45 +31,46 @@ def sample_sequence(*, hparams, length, address, start_token=None, batch_size=No
         # Don't feed the last context token -- leave that to the loop below
         # TODO: Would be slightly faster if we called step on the entire context,
         # rather than leaving the last token transformer calculation to the while loop.
-        context_output = step(hparams, context[:, :-1])
+        context_output = step (hparams, context [ : -1 ])
 
         def body(past, prev, output, addr):
-            next_outputs = step(hparams, prev[:, tf.newaxis], past=past)
-            logits = next_outputs['logits'][:, -1, :] # / tf . cast (temperature, dtype = tf . float32)
-            print (logits)
-            # NOTE: here im using that batch_size=1:
-            samples = tf . gather_nd (tf . argsort (logits [0, :], direction = "DESCENDING"), [ [ addr [ 0, 0 ] ], ])
-            #logits = top_k_logits(logits, k=top_k)
-            #samples = tf.multinomial(logits, num_samples=1, output_dtype=tf.int32)
-            # below is useless if batch_size = 1 everywhere
-            samples = samples [ : , tf . newaxis ]
-            #print (" from gatherd=" + str(samples))
-            samples . set_shape ((batch_size, 1))
-            #print (samples)
+            next_outputs = step (hparams, prev, past = past)
+            logits = next_outputs ['logits'] [:, -1, :]
+            samples = tf . gather_nd (tf . argsort (logits [0, :], direction = "DESCENDING"), [ [ addr [ 0 ] ], ])
+            #print (">>>> next past=" + str (tf.concat([past, next_outputs['presents']], axis=-2)))
+            #print (">>>> next prev=" + str(samples))
+            #print (">>>> next output="+str(tf.concat([output, samples], axis=0)))
+            #print (">>>> next addr="+str(addr[1:]))
             return [
                 tf.concat([past, next_outputs['presents']], axis=-2),
-                tf.squeeze(samples, axis=[1]),
-                tf.concat([output, samples], axis=1),
-                addr [ :, 1 : ],
+                samples,
+                tf.concat([output, samples], axis=0),
+                addr [ 1 : ],
             ]
 
         def cond(*args):
             return True
 
+        #print (">>>> past="+str(context_output["presents"]))
+        #print (">>>> prev="+str(context [ -1 : ]))
+        #print (">>>> output=context")
+        #print (">>>> context="+str(context))
+        #print (">>>> address="+str(address))
+
         _, _, tokens, _ = tf.while_loop(
             cond=cond, body=body,
             maximum_iterations=length,
             loop_vars=[
-                context_output['presents'],
-                context[:, -1],
+                context_output ['presents'],
+                context [ -1 : ],
                 context,
                 address,
             ],
             shape_invariants=[
-                tf.TensorShape(model.past_shape(hparams=hparams, batch_size=batch_size)),
-                tf.TensorShape([batch_size]),
-                tf.TensorShape([batch_size, None]),
-                tf . TensorShape ([batch_size, None]),
+                tf.TensorShape(model.past_shape(hparams=hparams, batch_size=1)),
+                tf.TensorShape([None, ]), # 1 really
+                tf.TensorShape([None, ]),
+                tf . TensorShape ([None, ]),
             ],
             back_prop=False,
         )
